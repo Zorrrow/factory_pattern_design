ab27a988f381dafbf389ed90ceebfb68a7e0b3bf
Zorrrow <110273869+Zorrrow@users.noreply.github.com>
2023-02-26 12:06:35 +0000

    Adding Travis badge

--- tests/scripts/vagrant_clean.sh
+++ tests/scripts/vagrant_clean.sh
@@ -13,4 +13,7 @@ done
 
 
 # Cleanup domain volumes
-for i in $(virsh vol-list
+for i in $(virsh vol-list default|grep \.img |grep -v VAGRANTSLASH | cut -f 2 -d ' ')
+do
+    virsh vol-delete "$i" --pool default
+done
